b2156143fef4a129abe88c85cae40f4fc9c6043f
AlbertoLyons <129765764+AlbertoLyons@users.noreply.github.com>
2025-01-06 20:14:39 -0300

    feat: added user model

---
+++ src/models/User.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catedra_3_Backend.src.models
+{
+    public class User
+    {
+        private string Email { get; set; } = "";
+        private string Password { get; set; } = "";
+    }
+}
