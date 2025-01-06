5a0569ba84ac51c3a928ae479d286308ed915b13
AlbertoLyons <129765764+AlbertoLyons@users.noreply.github.com>
2025-01-06 20:16:58 -0300

    chore: added .net packages

---
+++ src/controllers/UserController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catedra_3_Backend.src.controllers
+{
+    public class UserController
+    {
+        
+    }
+}
