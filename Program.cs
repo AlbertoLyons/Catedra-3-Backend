0e1e7d26a5e963e8f9e6ee5f137615f6ac916a7b
AlbertoLyons <alberto.lyons@alumnos.ucn.cl>
2025-01-06 23:46:24 -0300

    feat: added cloudinary

--- Program.cs
+++ Program.cs
@@ -5,15 +5,31 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using DotNetEnv;
-
 using Catedra_3_Backend.src.repositories;
-
 using Microsoft.EntityFrameworkCore;
 using System.Text;
+using Catedra_3_Backend.src.helpers;
+using CloudinaryDotNet;
 
 Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Crea las configuraciones de Cloudinary
+var cloudinarySettings = new CloudinarySettings
+{
+    CloudName = Environment.GetEnvironmentVariable("CLOUDINARY_CLOUD_NAME")!,
+    ApiKey = Environment.GetEnvironmentVariable("CLOUDINARY_API_KEY")!,
+    ApiSecret = Environment.GetEnvironmentVariable("CLOUDINARY_API_SECRET")!
+};
+// Crea la cuenta de Cloudinary
+var cloudinaryAccount = new Account(
+    cloudinarySettings.CloudName,
+    cloudinarySettings.ApiKey,
+    cloudinarySettings.ApiSecret
+);
+var cloudinary = new Cloudinary(cloudinaryAccount);
+builder.Services.AddSingleton(cloudinary);
 
 builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite("Data Source=Data.db"));
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
@@ -99,6 +115,8 @@ if (app.Environment.IsDevelopment())
 }
 app.UseCors("AllowAnyHost");
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
